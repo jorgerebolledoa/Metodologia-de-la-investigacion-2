18696ac8016eaf6b76db564f62a8b421173513f5
Getsuki53 <brian.gonzalez5353@gmail.com>
2025-09-06 14:28:30 -0400

    Codigo base, algunos comentarios en el txt

--- 1.c
+++ 1.c
@@ -13,14 +13,65 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <time.h>
+#include <string.h> 
 
 /*
  *
  */
 
+// Declaraciones de funciones
+unsigned long long factorialr(int n);
+unsigned long long factoriali(unsigned long long n);
+void FF1(int n, int k);
+void FF2(unsigned long long  n, unsigned long long  k);
+void Usage(char *mess);
+
+/*
+ *
+ */
+
+//Forma recursiva de primera formula
+void FF1(int i, int j) {
+   int resultado, n, k, m = 0;
+   n = factorialr(i);
+   k = factorialr(j);
+   m = factorialr(i - j);
+   resultado= n / (k * m);
+}
+
+//Forma recursiva de primera formula
+void FF2(unsigned long long i, unsigned long long j) {
+
+   unsigned long long resultado, n, k, m = 0;
+   n = factoriali(i);
+   k = factoriali(j);
+   m = factoriali(i - j);
+   if (k == 0 || m == 0) {
+      printf("Error: división por cero en FF1/FF2\n");
+      exit(1);
+   }
+   resultado= n / (k * m);
+}
+
+//Factorial recursivo
+unsigned long long factorialr(int n) {
+   if (n == 0)
+      return(1);
+   else
+      return(n * factorialr(n-1));
+}
+//Factorial iterativo
+unsigned long long factoriali(unsigned long long n) {
+   int i;
+   unsigned long long f = 1;
+   for (i = 1; i <= n; i++)
+      f = f * i;
+   return(f);
+}
+
 void Usage(char *mess) {
 
-   printf("\nUsage: %s mode n k\n",mess);
+   printf("\nUsage: %s -mode n k\n",mess);
    printf("\n mode in {FF1 (First Formula mode 1), SF1 (Second Formula mode 2) , etc}\n");
 }
 
@@ -30,35 +81,35 @@ void Usage(char *mess) {
 
 void main(int argc, char **argv) {
 
-   int n, k;
-   float E_cpu;
-   long E_wall;
-   time_t  ts, te;
-   clock_t csc, cec;
+    unsigned long long n, k;
+    float E_cpu;
+    long E_wall;
+    time_t  ts, te;
+    clock_t csc, cec;
 
 
-   if (argc == 4) {
+    if (argc == 4) {
       n = atoi(argv[2]);
       k = atoi(argv[3]);
       if (strcmp("-FF1",argv[1]) == 0) {
          csc = clock();
          ts = time(NULL);
-         //Aqui iria lo importante de la función
+         FF1(n,k);
          cec = clock();
          te = time(NULL);
          E_wall = (long)(te - ts);
          E_cpu = (float)(cec - csc) / CLOCKS_PER_SEC;
-//      printf("%d - Elapsed CPU Time %f Wall Time %ld\n",k,E_cpu,E_wall);
+         printf("%llu - Elapsed CPU Time %f Wall Time %ld\n",k,E_cpu,E_wall);
       }
-      if (strcmp("-SF1",argv[1]) == 0) {
+      if (strcmp("-FF2",argv[1]) == 0) {
          csc = clock();
          ts = time(NULL);
-         //Funcion a calcular tiempo
+         FF2(n,k);
          cec = clock();
          te = time(NULL);
          E_wall = (long)(te - ts);
          E_cpu = (float)(cec - csc) / CLOCKS_PER_SEC;
-//      printf("%d - Elapsed CPU Time %f Wall Time %ld\n",k,E_cpu,E_wall);
+         printf("%llu - Elapsed CPU Time %f Wall Time %ld\n",k,E_cpu,E_wall);
       }
       if (strcmp("-SF2",argv[1]) == 0) {
          csc = clock();
